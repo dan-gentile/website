f20517b857170534caee910e33090c52e5d5a2ac
Dan Gentile <dangentile@ymail.com>
2020-09-23 19:15:30 -0700

    basic layout and menu

---
+++ dist/main.js
@@ -0,0 +1,31 @@
+const menuBtn = $('.menu-btn');
+const hamburger = $('.menu-btn__burger');
+const nav = $('.nav');
+const menuNav = $('.menu-nav');
+const navItems = $('.menu-nav__item');
+
+let showMenu = false;
+
+menuBtn.click(toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        hamburger.addClass('open');
+        nav.addClass('open');
+        menuNav.addClass('open')
+        navItems.each(function () {
+            $(this).addClass('open')
+        })
+          
+
+        showMenu = true;
+    } else {
+        hamburger.removeClass('open');
+        nav.removeClass('open');
+        menuNav.removeClass('open')
+        navItems.each(function () {
+            $(this).removeClass('open')
+        })
+
+    }
+}
